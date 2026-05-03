4956467dd577d11cb6895db56940a02eb4efd27d
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-03 19:35:32 +0200

    feat: add for now current month as title in app layout

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,12 +1,24 @@
 import { CalendarProvider } from "@/src/providers/CalenderProvider";
+import { useTheme } from "@/src/providers/ThemeProvider";
 import { Stack } from "expo-router";
 import React from "react";
 
 export default function AppLayout() {
+  const { colors } = useTheme();
   return (
     <CalendarProvider>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            headerTitle: new Date().toLocaleDateString("default", {
+              month: "long",
+              year: "numeric",
+            }),
+            headerStyle: { backgroundColor: colors.background },
+            headerShadowVisible: false,
+          }}
+        />
         <Stack.Screen name="profile" options={{ title: "profile" }} />
       </Stack>
     </CalendarProvider>
