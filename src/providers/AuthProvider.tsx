6dd55901fda5b951ae6fb56fa29c4a1fb7dccc33
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-03 18:30:19 +0200

    chore: switch to using user and session types from supabase library

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,3 +1,4 @@
+import { Session, User } from "@supabase/supabase-js";
 import { createContext, use, type PropsWithChildren } from "react";
 import { Alert } from "react-native";
 import { supabase } from "../utils/supabase";
@@ -7,33 +8,36 @@ const AuthContext = createContext<{
   signIn: (email: string) => Promise<boolean>;
   verifyOtp: (email: string, token: string) => Promise<boolean>;
   signOut: () => void;
-  session?: string | null;
+  session: Session | null;
+  user: User | null;
   isLoading: boolean;
 }>({
   signIn: async () => false,
   verifyOtp: async () => false,
   signOut: () => null,
   session: null,
+  user: null,
   isLoading: false,
 });
 
-// Use this hook to access the user info.
 export function useAuth() {
   const value = use(AuthContext);
   if (!value) {
-    throw new Error("useSession must be wrapped in a <AuthProvider />");
+    throw new Error("useAuth must be wrapped in a <AuthProvider />");
   }
-
   return value;
 }
 
 export function AuthProvider({ children }: PropsWithChildren) {
-  const [[isLoading, session], setSession] = useStorageState("session");
+  const [[isLoading, sessionString], setSession] = useStorageState("session");
+
+  const session: Session | null = sessionString
+    ? JSON.parse(sessionString)
+    : null;
+  const user: User | null = session?.user ?? null;
 
   async function signIn(email: string): Promise<boolean> {
-    const { error } = await supabase.auth.signInWithOtp({
-      email,
-    });
+    const { error } = await supabase.auth.signInWithOtp({ email });
 
     if (error) {
       Alert.alert("Error", error.message);
@@ -69,6 +73,7 @@ export function AuthProvider({ children }: PropsWithChildren) {
         verifyOtp,
         signOut: () => setSession(null),
         session,
+        user,
         isLoading,
       }}
     >
