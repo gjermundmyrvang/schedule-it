b472ac7c01102270ceb87ff5b7bd7a4a6f463c3d
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-03 20:44:14 +0200

    chore: remove header from layout (will add to component instead)

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,20 +1,15 @@
 import { CalendarProvider } from "@/src/providers/CalenderProvider";
-import { useTheme } from "@/src/providers/ThemeProvider";
-import { getMonthString } from "@/src/utils/utils";
 import { Stack } from "expo-router";
 import React from "react";
 
 export default function AppLayout() {
-  const { colors } = useTheme();
   return (
     <CalendarProvider>
       <Stack>
         <Stack.Screen
           name="index"
           options={{
-            headerTitle: getMonthString(new Date()),
-            headerStyle: { backgroundColor: colors.background },
-            headerShadowVisible: false,
+            headerShown: false,
           }}
         />
         <Stack.Screen name="profile" options={{ title: "profile" }} />
