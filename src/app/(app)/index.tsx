66e47adec751ea7e7692a184ed77308f36ad2f17
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-03 18:32:29 +0200

    chore: create simple profile screen for logging out

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,5 +1,6 @@
 import { Text } from "@/src/components/Text";
 import { useTheme } from "@/src/providers/ThemeProvider";
+import { Link } from "expo-router";
 import { View } from "react-native";
 
 export default function Homepage() {
@@ -10,6 +11,7 @@ export default function Homepage() {
       className="flex-1 items-center justify-center"
     >
       <Text>Welcome to homescreen!</Text>
+      <Link href={"/profile"}>Profile</Link>
     </View>
   );
 }
