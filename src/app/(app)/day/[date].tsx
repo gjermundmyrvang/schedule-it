76cbf478c5b651b1262183c8742a68e3b7fc5ec7
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-04 16:26:23 +0200

    feat: navigate to create event modal if zero events, otherwise show events and button for navigating

--- src/app/(app)/day/[date].tsx
+++ src/app/(app)/day/[date].tsx
@@ -2,30 +2,31 @@ import { Text } from "@/src/components/Text";
 import { useCalendarContext } from "@/src/providers/CalenderProvider";
 import { getEventsForDay } from "@/src/utils/events";
 import { getFullDateString, parseDateParam } from "@/src/utils/utils";
-import { useLocalSearchParams } from "expo-router";
+import { Redirect, router, useLocalSearchParams } from "expo-router";
 import { Button, View } from "react-native";
 
 export default function DaySheet() {
   const { date } = useLocalSearchParams<{ date: string }>();
-  const { events, createEvent } = useCalendarContext();
+  const { events } = useCalendarContext();
 
   const dayDate = parseDateParam(date);
   const dayEvents = getEventsForDay(dayDate, events);
+
+  if (dayEvents.length <= 0) return <Redirect href={`/event/${date}`} />;
 
   return (
     <View className="flex-1 px-4 pt-6 min-h-125">
       <Text variant="title">{getFullDateString(dayDate)}</Text>
       {dayEvents.map((event) => (
-        <Text key={event.id}>{event.title}</Text>
+        <View key={event.id}>
+          <Text variant="title" className="text-base">
+            {event.title}
+          </Text>
+          <Text>Start: {new Date(event.starts_at).toLocaleTimeString()}</Text>
+          <Text>End: {new Date(event.ends_at).toLocaleTimeString()}</Text>
+        </View>
       ))}
-      <Button
-        title="Add test event"
-        onPress={() => {
-          const start = dayDate;
-          const end = new Date(dayDate.getTime() + 60 * 60 * 1000);
-          createEvent("Test Event", start, end);
-        }}
-      />
+      <Button title="New Event" onPress={() => router.push(`/event/${date}`)} />
     </View>
   );
 }
