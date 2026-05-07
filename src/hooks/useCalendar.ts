2321f23b892bd4609b4be10c6364c64f75370f69
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-07 20:18:20 +0200

    chore: let provider fetch and subscribe to calendars table

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -1,45 +1,10 @@
-import { useEffect, useState } from "react";
-import { Alert } from "react-native";
 import { useAuth } from "../providers/AuthProvider";
-import { Calendar } from "../types/supabase-types";
 import { supabase } from "../utils/supabase";
 
 export function useCalendars() {
   const { user } = useAuth();
-  const [calendars, setCalendars] = useState<Calendar[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetchCalendars();
-
-    const subscription = supabase
-      .channel("calendars")
-      .on(
-        "postgres_changes",
-        { event: "*", schema: "public", table: "calendars" },
-        fetchCalendars,
-      )
-      .subscribe();
-
-    return () => {
-      supabase.removeChannel(subscription);
-    };
-  }, []);
-
-  async function fetchCalendars() {
-    const { data, error } = await supabase
-      .from("calendars")
-      .select("*")
-      .order("created_at", { ascending: true });
-
-    if (error) Alert.alert("Error", error.message);
-    else setCalendars(data ?? []);
-
-    setIsLoading(false);
-  }
 
   async function createCalendar(name: string) {
-    console.log(user?.id);
     if (!user) return;
 
     const { error } = await supabase
@@ -55,5 +20,5 @@ export function useCalendars() {
     if (error) console.error(error);
   }
 
-  return { calendars, isLoading, createCalendar, deleteCalendar };
+  return { createCalendar, deleteCalendar };
 }
