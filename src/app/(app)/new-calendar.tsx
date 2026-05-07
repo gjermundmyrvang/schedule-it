c0b684f473612f1a9a8c6c3a3754641366d4199b
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-07 20:19:27 +0200

    feat: add formsheet for creating new calendars

--- src/app/(app)/new-calendar.tsx
+++ src/app/(app)/new-calendar.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "@/src/components/Text";
+import { useCalendars } from "@/src/hooks/useCalendar";
+import { useTheme } from "@/src/providers/ThemeProvider";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Button, TextInput, View } from "react-native";
 
 export default function NewCalendar() {
+  const { colors } = useTheme();
+  const { createCalendar } = useCalendars();
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleCreateCalendar = async () => {
+    if (name.trim() === "") {
+      Alert.alert("Name cannot be blank!");
+      return;
+    }
+    setLoading(true);
+    await createCalendar(name);
+    setLoading(false);
+    router.back();
+  };
+
   return (
-    <View className="flex-1 min-h-125">
-      <Text>NewCalendar</Text>
+    <View className="flex-1 h-screen px-4 pt-6">
+      <View>
+        <Text variant="title" className="text-3xl">
+          Create Calendar
+        </Text>
+        <TextInput
+          placeholder="My Shared Calendar..."
+          placeholderTextColor={colors.placeholderText}
+          value={name}
+          onChangeText={setName}
+          autoFocus
+          style={{
+            borderColor: colors.border,
+          }}
+          className="mt-4 w-full rounded-full border px-4 py-4"
+        />
+      </View>
+      <Button
+        title={loading ? "creating..." : "Create"}
+        onPress={handleCreateCalendar}
+      />
     </View>
   );
 }
